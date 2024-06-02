a936e2dca1249025e60a0f1c61464e9bb7c062fd
unknown <usamakhan58461@gmail.com>
2024-06-02 18:02:09 +0500

    variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+
+var firstname = "Im M.Usama Abbasi <br> <br>";
+document.write(firstname);
+
+
+let demp = "Computer Science <br> <br>";
+document.write(demp);
+
+
+const thar = ("Hobby is The Game ")
+document.write(thar);
